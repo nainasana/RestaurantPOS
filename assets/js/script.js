b73b7790283c42c2fc43746bde450162331a2792
NainaSana <nainasanask1987@gmail.com>
2024-03-06 17:18:16 +0530

    made responsive

--- assets/js/script.js
+++ assets/js/script.js
@@ -305,4 +305,22 @@ document.getElementById('branch5').addEventListener('click', function () {
 // });
 
 
+document.addEventListener('DOMContentLoaded', function () {
+  const boxes = document.querySelectorAll('.box');
 
+  window.addEventListener('scroll', checkBoxes);
+
+  function checkBoxes() {
+    const triggerBottom = (window.innerHeight / 5) * 4;
+
+    boxes.forEach((box) => {
+      const boxTop = box.getBoundingClientRect().top;
+
+      if (boxTop < triggerBottom) {
+        box.classList.add('show');
+      } else {
+        box.classList.remove('show');
+      }
+    });
+  }
+});
